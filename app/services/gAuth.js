efe768ca904ffd706a56947180deab9c0d073c94
SudharsunRavi <sudharsunr2004@gmail.com>
2025-10-07 10:07:44 +0530

    proxy

--- app/services/gAuth.js
+++ app/services/gAuth.js
@@ -2,23 +2,26 @@ import * as WebBrowser from 'expo-web-browser';
 import * as Google from 'expo-auth-session/providers/google';
 import { useEffect, useState } from 'react';
 import * as AuthSession from 'expo-auth-session';
+import { Platform } from 'react-native';
 
 WebBrowser.maybeCompleteAuthSession();
 
 export const useGoogleAuth = () => {
   const [token, setToken] = useState(null);
-  
-  //const redirectUri = 'https://auth.expo.io/@sudharsun/etrackerdrive';
-  //console.log('Redirect URI:', AuthSession.makeRedirectUri({ scheme: 'etrackerdrive' }));
+
+  const useProxy = Platform.OS !== 'web';
 
   const [request, response, promptAsync] = Google.useAuthRequest({
-    webClientId: '581076316369-nigghvi2i316q2mrr8kgf58nfbco2q0i.apps.googleusercontent.com',
     iosClientId: '581076316369-60fna26mnmsmipt2m9bqjc00h9qgpau0.apps.googleusercontent.com',
+    webClientId: '581076316369-nigghvi2i316q2mrr8kgf58nfbco2q0i.apps.googleusercontent.com',
     scopes: ['https://www.googleapis.com/auth/drive.file'],
-    redirectUri: AuthSession.makeRedirectUri({ scheme: 'etrackerdrive' }),
+    redirectUri: AuthSession.makeRedirectUri({
+      scheme: 'etrackerdrive',
+      useProxy,
+    }),
   });
 
-  console.log('Redirect URI:', AuthSession.makeRedirectUri({ scheme: 'etrackerdrive' }));
+  console.log('Redirect URI:', AuthSession.makeRedirectUri({ scheme: 'etrackerdrive', useProxy }));
 
   useEffect(() => {
     if (request) {
